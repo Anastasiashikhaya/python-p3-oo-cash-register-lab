ed2723a6e98541b059a241dfc0f93c83c6cd1509
Anastasia Wakoli <anastasiashikhaya@gmail.com>
2025-07-09 13:21:56 +0300

    ksh madam

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0.0
+        self.items = []
+        self.last_transaction = 0
+
+    def add_item(self, title, price, quantity=1):
+        self.last_transaction = price * quantity
+        self.total += self.last_transaction
+        self.items.extend([title] * quantity)
+
+    def apply_discount(self):
+        if self.discount == 0:
+            print("There is no discount to apply.")
+        else:
+            discount_amount = self.total * self.discount / 100
+            self.total -= discount_amount
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction
+        # Remove the items from the last transaction
+        if self.last_transaction > 0:
+            # For simplicity, we'll just clear all items if total is 0
+            if self.total == 0:
+                self.items = []
+            # In a more complete implementation, we'd track items per transaction
